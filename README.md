451a55d4663d00cec9f5e4926dd344941b754e57
xiaohui.lam <xiaohui.lam@icloud.com>
2019-06-12 11:18:24 +0800

    add english test

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-!未成熟的包, 勿用.
+**未成熟的包, 勿用.**
+
+**Unrelease package, don't use it on production.**
---
+++ tests/english.php
@@ -0,0 +1,6 @@
+<?php
+use Xiaohuilam\LaravelTimePattern\Pattern;
+
+require __DIR__ . '/boot.php';
+
+dump(Pattern::parse('I met you before last year'));
